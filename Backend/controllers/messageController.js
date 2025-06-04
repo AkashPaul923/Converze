15357f8442094fba34aad1436c5f9fde4cf3c3d2
AkashPaul923 <akashpaul923@gmail.com>
2025-06-04 21:19:29 +0600

    message get endpoint

--- Backend/controllers/messageController.js
+++ Backend/controllers/messageController.js
@@ -38,3 +38,18 @@ export const sendMessage = async (req, res) => {
         console.log(error);
     }
 };
+
+export const getMessage = async (req, res) => {
+    try {
+        const senderId = req.id;
+        const reciverId = req.params.id;
+
+        const conversation = await Conversation.findOne({
+            participants: { $all: [senderId, reciverId] },
+        }).populate("messages");
+
+        return res.status(201).json(conversation?.messages);
+    } catch (error) {
+        console.log(error);
+    }
+};

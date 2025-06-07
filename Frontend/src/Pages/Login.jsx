371b0bea3371831885bff8125a98e77fee24e7e2
AkashPaul923 <akashpaul923@gmail.com>
2025-06-07 11:57:37 +0600

    signup and login form design

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+    return (
+        <div className="min-h-screen flex justify-center items-center">
+            <form className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                <legend className="fieldset-legend">Login</legend>
 
-export default Login
+                <label className="label">User Name</label>
+                <input type="text" className="input" placeholder="username" />
+
+                <label className="label">Password</label>
+                <input
+                    type="password"
+                    className="input"
+                    placeholder="Password"
+                />
+
+                <button className="btn btn-neutral mt-4">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;

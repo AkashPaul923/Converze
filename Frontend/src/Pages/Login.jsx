adb7c7c9ae4993e1248def008a69b2f7d6987ba6
AkashPaul923 <akashpaul923@gmail.com>
2025-08-01 22:38:20 +0600

    login successfully

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -1,33 +1,75 @@
-
-import { Link } from "react-router";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Link, useNavigate } from "react-router";
 
 const Login = () => {
+    const navigate = useNavigate();
 
-    const onSubmitHandler = (e) => {
+    const onSubmitHandler = async (e) => {
         e.preventDefault();
         const form = e.target;
         const username = form.username.value;
         const password = form.password.value;
+        const user = {
+            username,
+            password
+        };
 
-        console.log({username, password});
-    }
-
+        try {
+            const res = await axios.post(
+                "http://localhost:5000/api/v1/user/login",
+                user,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    withCredentials: true,
+                }
+            );
+            // console.log(res)
+            if (res.status === 200) {
+                toast.success("Login Successfully");
+                navigate("/");
+            }
+        } catch (error) {
+            // console.log(error);
+            toast.error(error.response.data.message);
+        }
+    };
 
     return (
         <div className="flex justify-center items-center h-screen">
             <div className="p-7 backdrop-blur-xs w-100 border rounded-2xl">
-                <h1 className="text-3xl font-semibold mb-5 text-center">Login</h1>
-                <form onSubmit={onSubmitHandler} className="flex flex-col gap-3">
+                <h1 className="text-3xl font-semibold mb-5 text-center">
+                    Login
+                </h1>
+                <form
+                    onSubmit={onSubmitHandler}
+                    className="flex flex-col gap-3"
+                >
                     <div>
                         <label className="label block mb-1.5">Username</label>
-                        <input type="text" name="username" className="input w-full" placeholder="Username" required/>
+                        <input
+                            type="text"
+                            name="username"
+                            className="input w-full"
+                            placeholder="Username"
+                            required
+                        />
                     </div>
                     <div>
                         <label className="label block mb-1.5">Password</label>
-                        <input type="password" name="password" className="input w-full" placeholder="Password" required/>
+                        <input
+                            type="password"
+                            name="password"
+                            className="input w-full"
+                            placeholder="Password"
+                            required
+                        />
                     </div>
-                    <div >
-                        Don't have any account?<Link to="/signup"> Create account.</Link>
+                    <div>
+                        Don't have any account?
+                        <Link to="/signup"> Create account.</Link>
                     </div>
                     <button className="btn btn-block btn-neutral">Login</button>
                 </form>
